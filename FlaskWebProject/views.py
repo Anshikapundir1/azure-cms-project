47cea5e25e403f111250905fc85f9c99026979c9
Anshikapundir1 <161428274+Anshikapundir1@users.noreply.github.com>
2026-03-17 01:04:32 +0530

    views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -12,6 +12,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
 import uuid
+import logging
+from flask import current_app as app
 
 imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
 
@@ -69,6 +71,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info(f"User {user.username} logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
